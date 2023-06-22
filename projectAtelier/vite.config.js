700d78a9e14f5135d54a1fb0cea88b9e894bd293
Kiel Fuller <kpfuller28@gmail.com>
2023-06-22 16:30:11 -0400

    axios, kill port, vitest and react testing library initialized

--- projectAtelier/vite.config.js
+++ projectAtelier/vite.config.js
@@ -1,7 +1,16 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    global: true,
+    coverage: {
+      provider: 'v8'
+    },
+    setupFiles: './tests/setup.js',
+  },
 })
