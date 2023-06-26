0683ff8f37fa138f841ea74d5996da66e589eb22
Akash Rajan <akashrajan93@gmail.com>
2023-06-26 08:57:20 -0400

    setup complete for ProductDetails axios fetch

--- projectAtelier/client/src/App.jsx
+++ projectAtelier/client/src/App.jsx
@@ -1,17 +1,18 @@
+/* eslint-disable no-unused-vars */
 import {useEffect, useState} from 'react';
 import axiosGet from './apiRoutes.js';
-import RelatedProducts from './components/RelatedProducts.jsx';
-import ProductDetails from './components/ProductDetails.jsx';
+import RelatedProducts from './components/RelatedProducts/RelatedProducts.jsx';
+import ProductDetails from './components/Product_Details/ProductDetails.jsx';
 import RatingsReviews from './components/Ratings_Review/RatingsReviews.jsx';
 
 function App() {
   const [product, setProduct] = useState({});
 
-useEffect(() => {
-  axios.get(getConfig.url + '/products', getConfig).then((response) => {
-    setProduct(response.data[0]);
-  })
-},[])
+// useEffect(() => {
+//   axios.get(getConfig.url + '/products', getConfig).then((response) => {
+//     setProduct(response.data[0]);
+//   })
+// },[])
 
   return (
     <div>
