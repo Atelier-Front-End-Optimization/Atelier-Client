0e69af27880a0a141e78f137db73212ee86ce412
Akash Rajan <akashrajan93@gmail.com>
2023-06-27 15:28:34 -0400

    adding styles buttons and aligning components

--- projectAtelier/client/src/components/Product_Details/Product_Subcomps/ProductStyles.jsx
+++ projectAtelier/client/src/components/Product_Details/Product_Subcomps/ProductStyles.jsx
@@ -1,11 +1,28 @@
 // eslint-disable-next-line no-unused-vars
 import {useState, useEffect} from 'react';
+import Box from '@mui/material/Grid'
 
-const ProductStyles = () => {
+const ProductStyles = ({styles, photos}) => {
 
   return (
-    <div>PRODUCT STYLES GO HERE</div>
+<Box>
+  <ul>
+    {photos.map((photo) => {
+      return (
+        <button key={Math.random()} type="button">
+            <img
+              src={photo.url}
+              alt="Product Image"
+              style={{ borderRadius: '100%', width: '75px', height: '75px', borderColor:'white'}}
+            />
+          </button>
+
+    )}
+  )}
+  </ul>
+  </Box>
   )
-}
+  }
+
 
 export default ProductStyles;
