dba44f8750729bd3f66b680d73ecf3f3af650a89
Akash Rajan <akashrajan93@gmail.com>
2023-06-30 10:31:51 -0400

    fixed Vite cache issue

--- projectAtelier/client/src/components/RelatedProducts/RelatedSubs/OutfitList.jsx
+++ projectAtelier/client/src/components/RelatedProducts/RelatedSubs/OutfitList.jsx
@@ -1,51 +1,47 @@
 import OutfitCard from './OutfitCard.jsx';
-import Box from '@mui/material/Box';
-import {useEffect, useState} from 'react';
-import Carousel from "react-multi-carousel";
+import { Box } from '@mui/material/';
+import { useEffect, useState } from 'react';
+import Carousel from 'react-multi-carousel';
 import AddOutfit from './AddOutfit.jsx';
 import '../../../index.css';
-
-
 
 const responsive = {
   superLargeDesktop: {
     // the naming can be any, depends on you.
     breakpoint: { max: 4000, min: 3000 },
-    items: 5
+    items: 5,
   },
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
-    items: 3
+    items: 3,
   },
   tablet: {
     breakpoint: { max: 1024, min: 464 },
-    items: 2
+    items: 2,
   },
   mobile: {
     breakpoint: { max: 464, min: 0 },
-    items: 1
-  }
+    items: 1,
+  },
 };
 
-function OutfitList({currentProduct}) {
+function OutfitList({ currentProduct }) {
   const [outfit, setOutfit] = useState([null]);
   // console.log(outfit)
   return (
     <div>
       <div>Outfit List</div>
-        <Box className='carousel-box'>
-          <Carousel itemClass='carousel-item' responsive={responsive}>
-              {outfit.map((product) => {
-                  if (!product) {
-                    return <AddOutfit/>
-                  } else {
-                    return <OutfitCard key={product.id} handleClick={handleClick} />
-                  }
-
-              })}
-          </Carousel>
-        </Box>
-
+      <Box className="carousel-box">
+        <Carousel itemClass="carousel-item" responsive={responsive}>
+          {outfit.map((product) => {
+            if (!product) {
+              return <AddOutfit />;
+            } else {
+              return <OutfitCard key={product.id} handleClick={handleClick} />;
+            }
+          })}
+        </Carousel>
+      </Box>
     </div>
   );
 }
--- projectAtelier/client/src/components/RelatedProducts/RelatedSubs/RelatedList.jsx
+++ projectAtelier/client/src/components/RelatedProducts/RelatedSubs/RelatedList.jsx
@@ -1,99 +1,121 @@
 import axios from 'axios';
 import axiosConfig from '../../../Middleware/axiosConfig.js';
-import Box from '@mui/material/Box';
-import Carousel from "react-multi-carousel";
+import { Box } from '@mui/material/';
+import Carousel from 'react-multi-carousel';
 import RelatedCard from './RelatedCard.jsx';
-import "react-multi-carousel/lib/styles.css";
+import 'react-multi-carousel/lib/styles.css';
 import AddOutfit from './AddOutfit.jsx';
 import '../../../index.css';
 
 const responsive = {
   evenBiggerDesktop: {
-    breakpoint: {max: 2500, min: 2000},
-    items: 5
+    breakpoint: { max: 2500, min: 2000 },
+    items: 5,
   },
   superLargeDesktop: {
     // the naming can be any, depends on you.
     breakpoint: { max: 2000, min: 1500 },
-    items: 4
+    items: 4,
   },
   desktop: {
     breakpoint: { max: 1500, min: 1024 },
-    items: 3
+    items: 3,
   },
   tablet: {
     breakpoint: { max: 1024, min: 464 },
-    items: 2
+    items: 2,
   },
   mobile: {
     breakpoint: { max: 464, min: 0 },
-    items: 1
-  }
+    items: 1,
+  },
 };
 
-
-
-
-function RelatedList({currentProduct, setProduct, products, list}) {
-
-
+function RelatedList({ currentProduct, setProduct, products, list }) {
   //set the current product when a related card is clicked
   function relatedClick(id) {
-    axios.get(axiosConfig.url + '/products/' + id, axiosConfig).then((response) => {
-      setProduct(response.data);
-    })
+    axios
+      .get(axiosConfig.url + '/products/' + id, axiosConfig)
+      .then((response) => {
+        setProduct(response.data);
+      });
   }
 
   function addOutfit(product) {
     let ids = [];
-      for (let obj of  products) {
-        if (obj) {
-          ids.push(Object.values(obj)[0])
-        }
+    for (let obj of products) {
+      if (obj) {
+        ids.push(Object.values(obj)[0]);
       }
+    }
     if (ids.length === 0 || !ids.includes(product.id)) {
-      setProduct(products => [...products, product])
+      setProduct((products) => [...products, product]);
     }
   }
 
   function deleteOutfit(id) {
-    console.log('I was clicked ', id)
+    console.log('I was clicked ', id);
   }
-
-
 
   if (list === 'related') {
     return (
-    <div>
-      <div className='related-outfit-header'>Related Products</div>
-        <Box className='carousel-box'>
-          <Carousel itemClass='carousel-item' responsive={responsive}  draggable={false}>
-              {products.map((product) => {
-                  return <RelatedCard key={product.id} product={product} currentProduct={currentProduct} handleClick={relatedClick} list={list} />
-              })}
+      <div>
+        <div className="related-outfit-header">Related Products</div>
+        <Box className="carousel-box">
+          <Carousel
+            itemClass="carousel-item"
+            responsive={responsive}
+            draggable={false}
+          >
+            {products.map((product) => {
+              return (
+                <RelatedCard
+                  key={product.id}
+                  product={product}
+                  currentProduct={currentProduct}
+                  handleClick={relatedClick}
+                  list={list}
+                />
+              );
+            })}
           </Carousel>
         </Box>
-
-    </div>
+      </div>
     );
   } else {
     return (
       <div>
-      <div className='related-outfit-header'>Outfit</div>
-        <Box className='carousel-box'>
-          <Carousel itemClass='carousel-item' responsive={responsive} draggable={false}>
-              {products.map((product, index) => {
-                  if (!product) {
-                    return <AddOutfit key={index} handleClick={addOutfit} currentProduct={currentProduct}/>
-                  } else {
-                    return <RelatedCard key={product.id} product={product} currentProduct={currentProduct} handleClick={deleteOutfit} list={list}/>
-                  }
-
-              })}
+        <div className="related-outfit-header">Outfit</div>
+        <Box className="carousel-box">
+          <Carousel
+            itemClass="carousel-item"
+            responsive={responsive}
+            draggable={false}
+          >
+            {products.map((product, index) => {
+              if (!product) {
+                return (
+                  <AddOutfit
+                    key={index}
+                    handleClick={addOutfit}
+                    currentProduct={currentProduct}
+                  />
+                );
+              } else {
+                return (
+                  <RelatedCard
+                    key={product.id}
+                    product={product}
+                    currentProduct={currentProduct}
+                    handleClick={deleteOutfit}
+                    list={list}
+                  />
+                );
+              }
+            })}
           </Carousel>
         </Box>
-
-    </div>
+      </div>
     );
   }
 }
