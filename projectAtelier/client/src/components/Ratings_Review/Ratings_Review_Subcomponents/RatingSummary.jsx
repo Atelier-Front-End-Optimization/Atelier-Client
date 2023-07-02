c6f881822271bb9b0d60c221a2c8e88c214bcc5a
d-verhnjak <darko.verhnjak@gmail.com>
2023-07-01 21:48:38 -0400

    Average rating obtained

--- projectAtelier/client/src/components/Ratings_Review/Ratings_Review_Subcomponents/RatingSummary.jsx
+++ projectAtelier/client/src/components/Ratings_Review/Ratings_Review_Subcomponents/RatingSummary.jsx
@@ -1,13 +1,25 @@
+/* eslint-disable react/prop-types */
 import {useEffect, useState} from 'react';
+import { Box, Rating, Button} from '@mui/material';
+import averageRating from "../../../Middleware/averageRating.js";
 
-const RatingSummary = () => {
+const RatingSummary = ({ ratings}) => {
+  // const [avgRating, setAvgRating] = useState(0);
 
+  const avgRating = averageRating(ratings)
+  console.log(avgRating)
 
 ////////////////////////////////////////////////////////
   return (
-    <div>
-      RatingSummary
-    </div>
+    <Box>
+    {/* //    <Rating */}
+    {/* //       name="Item Review"
+    //       value={rating}
+    //       precision={0.25}
+    //       size='small'
+    //       readOnly
+    //     /> */}
+    </Box>
   );
 };
 
