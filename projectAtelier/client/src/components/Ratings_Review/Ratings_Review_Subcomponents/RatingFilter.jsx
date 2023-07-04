6ba98bb0047dcd6247e653fdaa23f8fedc8e9a5a
d-verhnjak <darko.verhnjak@gmail.com>
2023-07-04 12:23:11 -0400

    links and bars align

--- projectAtelier/client/src/components/Ratings_Review/Ratings_Review_Subcomponents/RatingFilter.jsx
+++ projectAtelier/client/src/components/Ratings_Review/Ratings_Review_Subcomponents/RatingFilter.jsx
@@ -3,11 +3,9 @@ import {useEffect, useState} from 'react';
 import {
   Chart,
   BarSeries,
-  ArgumentAxis,
-  ValueAxis,
 } from '@devexpress/dx-react-chart-material-ui';
 import { Animation } from '@devexpress/dx-react-chart';
-
+import { Box, Stack, Link } from '@mui/material';
 
 const RatingFilter = ({ ratingsData }) => {
 
@@ -18,18 +16,106 @@ const RatingFilter = ({ ratingsData }) => {
 
     <div>
       { ratingsData.length > 0 ?
-        <Chart
-            data={ratingsData}
-            rotated
-          >
-            {/* <ArgumentAxis /> */}
+        <Stack
+        style={{
+          paddingTop: '2px',
+          paddingBottom: '2px'
+        }}
+          direction='row'
+        >
+          <Stack
+          style={{
+            paddingTop: '90px'
+          }}
+            direction="column"
+            justifyContent="space-evenly"
+            alignItems="flex-start"
+            spacing={1}
+            sx={{
+              height: "22px",
+              width: '100px',
 
-            <BarSeries
-              valueField="votes"
-              argumentField="stars"
-            />
-            <Animation />
-          </Chart>
+            }}
+          >
+            <div
+              style={{
+                marginTop: '22.5px',
+                marginLeft: '25px'
+              }}
+            >
+              <Link
+                sx={{textDecoration: 'underline'}}
+              >
+                5 stars
+              </Link>
+            </div>
+            <div
+              style={{
+                marginTop: '22.5px',
+                marginLeft: '25px'
+              }}
+            >
+              <Link
+                sx={{textDecoration: 'underline'}}>
+                4 stars
+              </Link >
+            </div>
+            <div
+              style={{
+                marginTop: '22.5px',
+                marginLeft: '25px'
+              }}
+            >
+              <Link sx={{textDecoration: 'underline'}}>
+                3 stars
+              </Link>
+            </div>
+            <div
+              style={{
+                marginTop: '22.5px',
+                marginLeft: '25px'
+              }}
+            >
+              <Link sx={{textDecoration: 'underline'}}>
+                2 stars
+              </Link>
+            </div>
+            <div
+              style={{
+                marginTop: '22.5px',
+                marginLeft: '25px'
+              }}
+            >
+              <Link sx={{textDecoration: 'underline'}}>
+                1 stars
+              </Link>
+            </div>
+          </Stack>
+
+          <Stack
+            style = {{
+              paddingTop: '0px',
+              paddingBottom: '10px'
+            }}
+            sx={{
+              height: "220px",
+              width: '185px'
+            }}
+          >
+            <Chart
+                data={ratingsData}
+                rotated
+              >
+              <BarSeries
+                valueField="votes"
+                argumentField="stars"
+              />
+              <Animation />
+            </Chart>
+
+          </Stack>
+        </Stack>
+
       : ''}
     </div>
   );
