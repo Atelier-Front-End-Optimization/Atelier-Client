bbcb65ec963d84eb9ca16c716ce2d4daca1f7a25
d-verhnjak <darko.verhnjak@gmail.com>
2023-07-07 00:08:20 -0400

    FIN

--- projectAtelier/client/src/components/Ratings_Review/Ratings_Review_Subcomponents/ProductBreakdown.jsx
+++ projectAtelier/client/src/components/Ratings_Review/Ratings_Review_Subcomponents/ProductBreakdown.jsx
@@ -1,7 +1,5 @@
 /* eslint-disable react/prop-types */
-import { useEffect, useState } from 'react';
 import { Slider, Box } from '@mui/material';
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
 const ProductBreakdown = ({ characteristics }) => {
 
